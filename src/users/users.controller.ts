890d019125a516bbee2733d18a0808808df811c3
jahona <jahona5240@gmail.com>
2022-08-29 15:42:55 +0900

    유저 서비스 인터페이스 수정

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,41 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { GetUsersDto } from './dto/get-user-dto';
+import { UserLoginDto } from './dto/user-login.dto';
+import { VerifyEmailDto } from './dto/verify-email.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
-
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    const { name, email } = createUserDto;
-    console.log(`유저를 생성했습니다. 이름: ${name}, 이메일: ${email}`);
-
-    return this.usersService.create(createUserDto);
+  async createUser(@Body() dto: CreateUserDto): Promise<void> {
+    console.log(dto);
+    return;
   }
 
-  @Get()
-  findAll(@Query() getUsersDto: GetUsersDto) {
-    const { offset, limit } = getUsersDto;
-    console.log(`유저를 생성했습니다. 페이지번호: ${offset}, 페이지개수: ${limit}`);
-
-    return this.usersService.findAll();
+  @Post('/email-verify')
+  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+    console.log(dto);
+    return;
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  @Post('/login')
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    console.log(dto);
+    return;
   }
 }
