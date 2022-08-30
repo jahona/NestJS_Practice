041fd7f2274cec9115155faf2b3d2ad66527449f
jahona <jahona5240@gmail.com>
2022-08-30 11:44:45 +0900

    nest g service Users, nest g controller Users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class AppModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,41 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { GetUsersDto } from './dto/get-user-dto';
+import { Controller } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
-
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    const { name, email } = createUserDto;
-    console.log(`유저를 생성했습니다. 이름: ${name}, 이메일: ${email}`);
-
-    return this.usersService.create(createUserDto);
-  }
-
-  @Get()
-  findAll(@Query() getUsersDto: GetUsersDto) {
-    const { offset, limit } = getUsersDto;
-    console.log(`유저를 생성했습니다. 페이지번호: ${offset}, 페이지개수: ${limit}`);
-
-    return this.usersService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
-  }
-}
+export class UsersController {}
