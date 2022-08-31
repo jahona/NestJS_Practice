e9a9b2df06554080f0b89d867a3881ee42eea5cb
jahona <jahona5240@gmail.com>
2022-08-31 12:02:16 +0900

    환경변수 모듈 구성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { EmailService } from './email/email.service';
 import { EmailModule } from './email/email.module';
+import { ConfigModule } from '@nestjs/config';
+import emailConfig from './config/emailConfig';
+import { validationSchema } from './config/validationSchema';
 
 @Module({
-  imports: [UsersModule, EmailModule],
+  imports: [
+    UsersModule, 
+    ConfigModule.forRoot({
+      // envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      envFilePath: [`${__dirname}/config/env/.development.env`],
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema,
+    }),
+  ],
   controllers: [AppController],
-  // providers: [AppService, EmailService],
   providers: [AppService],
 })
 export class AppModule {}
--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import emailConfig from '../config/emailConfig';
+import { ConfigType } from '@nestjs/config';
 
 interface EmailOptions {
     to: string;
@@ -8,12 +10,12 @@ interface EmailOptions {
 
 @Injectable()
 export class EmailService {
-    constructor() {
-
+    constructor(@Inject(emailConfig.KEY) private config: ConfigType<typeof emailConfig>) {
+        console.log(`emailConfig: ${JSON.stringify(config)}`);
     }
 
     async sendMemberJoinVerification(email: string, signupVerifyToken: string) {
-        const baseUrl = 'http://localhost:3000'; // TODO: config
+        const baseUrl = this.config.baseUrl;
 
         const url = `${baseUrl}/users/email-verify?signupVerifyToken=${signupVerifyToken}`;
 
